6cc84ecf257b698c4a9cd902cfb8fed9ff307a32
pandemoniummm <mefucto77@gmail.com>
2022-05-23 15:41:12 +0300

    tests added

--- LiepaLimitedTest.Test/Program.cs
+++ LiepaLimitedTest.Test/Program.cs
@@ -1,43 +1,107 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LiepaLimitedTest.Test
 {
     internal class Program
     {
+        private static readonly string Host = "https://localhost:44384";
         private static readonly HttpClient client = new HttpClient();
-        static void Main(string[] args)
+        private static readonly List<string>  Statuses = new List<string> { "New", "Active", "Blocked", "Deleted" };
+    static void Main(string[] args)
         {
-            var responseString = client.GetStringAsync("https://localhost:55002/user?id=1").Result;
-            Console.WriteLine(responseString);
-            //Task.Run(async () =>
-            //{
-            //    while (true)
-            //    {
-            //        var responseString = await client.GetStringAsync("https://localhost:55000/user?id=1");
-            //        Console.WriteLine(responseString);
-            //        await Task.Delay(1000);
-            //    }
-            //});
+            Task.Run(async () =>
+            {
+                var random = new Random();
+                while (true)
+                {
+                    var item = random.Next(0, 100);
+                    var responseString = "";
+                    try
+                    {
+                        responseString = await client.GetStringAsync(Host + "/user?id=" + item);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Select error  " + ex.Message);
+                        continue;
+                    }                  
+                    Console.WriteLine("Selected :" + item + @"\r\n\" + responseString);
+                    await Task.Delay(100);
+                }
+            });
 
-            //Task.Run(async () =>
-            //{
-            //    while (true)
-            //    {
-            //        Console.WriteLine("Thread2");
-            //        await Task.Delay(1000);
-            //    }
-            //});
+            Task.Run(async () =>
+            {
+                var random = new Random();
+                var i = 1;
+                while (true)
+                {                    
+                    var values = new Dictionary<string, string>
+                    {
+                        { "name", "user" + i },
+                        { "status", Statuses[random.Next(0,4)] }
+                    };
 
-            //Task.Run(async () =>
-            //{
-            //    while (true)
-            //    {
-            //        Console.WriteLine("Thread3");
-            //        await Task.Delay(1000);
-            //    }               
-            //});
+                    var content = new StringContent(JsonConvert.SerializeObject(values), Encoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync(Host + "/user", content);
+
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    Console.WriteLine($"Created: {i}  {responseString}");
+
+                    i++;
+                    await Task.Delay(300);
+                }
+            });
+
+            Task.Run(async () =>
+            {
+                var random = new Random();
+                var i = 1;
+                while (true)
+                {
+                    var responseString = "";
+                    try
+                    {
+                        responseString = await client.GetStringAsync(Host + "/user?id=" + random.Next(0, 200));
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Select for update failed " + ex.Message);
+                        continue;
+                    }
+
+                    Console.WriteLine(responseString);
+
+                    var values = new Dictionary<string, string>
+                    {
+                        { "name", "updated" + i },
+                        { "status", Statuses[random.Next(0,4)] }
+                    };
+
+                    var content = new StringContent(JsonConvert.SerializeObject(values), Encoding.UTF8, "application/json");
+
+                    var response = await client.PostAsync(Host + "/user/status", content);
+
+                    if (response.StatusCode == System.Net.HttpStatusCode.InternalServerError)
+                    {
+                        Console.WriteLine($"Could not update: " + i);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Updated succesfully: " + i + @"\r\n\" + await response.Content.ReadAsStringAsync());
+                    }
+
+                    await Task.Delay(300);
+                }
+            });
 
             while (true)
             {
